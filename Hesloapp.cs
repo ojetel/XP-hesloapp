5f3b6c86d02b59de85400928072eddf1048cc5b1
Ondřej Jetel <ondrej.jetel@gmail.com>
2025-03-27 20:59:58 +0100

    Add files via upload

---
+++ Hesloapp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    internal class Hesloapp
+    {
+        public static string heslo = "";
+
+        public static bool ObsahujeRPR(string heslo)
+        {
+            return heslo.Contains("RPR");
+        }
+        public static bool JeDlouhe(string heslo)
+        {
+            if (heslo.Length >= 8 && heslo.Length <= 20)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool JeNeprazdne(string heslo)
+        {
+            if(heslo.Length > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
